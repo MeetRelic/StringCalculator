0d27572e5945f42575807d13b7fcb3f348811c2a
MeetRelic <meetngandhi@gmail.com>
2021-03-22 11:12:54 +0530

    Task 3 Completed .

--- Calculator/src/main/java/com/StringCalulator/Calculator.java
+++ Calculator/src/main/java/com/StringCalulator/Calculator.java
@@ -9,9 +9,9 @@ public class Calculator {
 		//first we check if string is empty || Null we return 0
 		if(input.isEmpty()) {
 			return 0;
-		}else if(input.contains(",")) {
-			//if we get comma now we know as "," there is two number and our tast is to return sum of it , so will return!!
-		    String [] splitfromcomma = input.split(",");
+		}else if(input.contains(",") || input.contains("|")) {
+			//if we get comma now we know as "," or "\n"  there is n number and our test is to return sum of it , so will return!!
+		    String [] splitfromcomma = input.split(",|\n");
 		    int ans = 0;
 		   for(String s : splitfromcomma) { // as there can be n number of number in comma
 		    	ans += Integer.parseInt(s);
--- Calculator/src/main/java/com/StringCalulator/TestCalculator.java
+++ Calculator/src/main/java/com/StringCalulator/TestCalculator.java
@@ -15,12 +15,19 @@ public class TestCalculator {
 	public void Task1Calcultor_ShouldreturnInteger() {
 		assertEquals(5, Calculator.CalculatorTasks("5"));
 	}
+	//Task1 Ends
     @Test
     public void Task1Calcultor_ShouldcheckComma() {
 		assertEquals(10, Calculator.CalculatorTasks("3,7"));
 	}
+    //Task 2 Starts - for n number of numbers is handled
     @Test
     public void Task1Calcultor_ShouldcheckMultipleComma() {
 		assertEquals(44, Calculator.CalculatorTasks("3,7,8,7,9,10"));
+	}
+    //Task 3 starts - allow "\n" in place of comma 
+    @Test
+    public void Task1Calcultor_ShouldcheckMultipleCommaandNewline() {
+		assertEquals(10, Calculator.CalculatorTasks("1,2,3\n4"));
 	}
 }
