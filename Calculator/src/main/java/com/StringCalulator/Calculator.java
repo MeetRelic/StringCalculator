ca68d3291429300233f2a224ef80464db718fddd
MeetRelic <meetngandhi@gmail.com>
2021-03-22 10:42:33 +0530

    Task1 Small Updates and Relevant TestcasesPassed Do Review !!

--- Calculator/src/main/java/com/StringCalulator/Calculator.java
+++ Calculator/src/main/java/com/StringCalulator/Calculator.java
@@ -1,5 +1,19 @@
 package com.StringCalulator;
 
-public class Calculator {
 
+
+public class Calculator {
+	//This Method is main String Calculator Method for 
+	
+	public static int CalculatorTasks(String input) {
+		//first we check if string is empty || Null we return 0
+		if(input.isEmpty()) {
+			return 0;
+		}
+		else {
+			//if not null and only one value in string we return directly Same int 
+			return Integer.parseInt(input);
+		}
+		
+	}
 }
--- Calculator/src/main/java/com/StringCalulator/TestCalculator.java
+++ Calculator/src/main/java/com/StringCalulator/TestCalculator.java
@@ -1,5 +1,20 @@
 package com.StringCalulator;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+//This is Junit Testing class for String Calculator Method.
 public class TestCalculator {
+    //Task 1 starts here....
+	@Test
+	public void Task1Calcultor_ShouldverifyNull() {
+	assertEquals(0, Calculator.CalculatorTasks(""));
+	}
+	@Test
+	public void Task1Calcultor_ShouldreturnInteger() {
+		assertEquals(5, Calculator.CalculatorTasks("5"));
+	}
+    
 
 }
